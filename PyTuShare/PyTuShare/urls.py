b444edd0d2ba0a7b5091ae94516521ce85da469f
juno <yuyezhulan@163.com>
2015-07-12 02:42:55 -0700

    init program

--- PyTuShare/PyTuShare/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from PyTuShare.views import hello
-
-urlpatterns = [
-#     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', hello),
-
-]
